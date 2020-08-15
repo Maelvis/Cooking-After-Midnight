d4dda4b2b912c8fde740cd1a502e8ba17d7f52b5
Maelvis <63719019+Maelvis@users.noreply.github.com>
2020-08-15 13:13:11 +0100

    Ingredients and Method tabs
    Complete ingredients and method tabs.
    
    -CSS styling for open / closed tabs
    - Implementation of tabs on the rest of the recipes using appropriate divs

--- recipes.js
+++ recipes.js
@@ -4,19 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 let methodTab = document.getElementsByClassName('methodtab');
 let ingredientsTab = document.getElementsByClassName('ingredientstab');
+let methodButton = document.getElementsByClassName('methodbutton');
+let ingredientsButton = document.getElementsByClassName('ingredientsbutton');
 
 
 // Opening the method tab
 function openMethod() {
     for (i = 0; i < ingredientsTab.length; i++) {
         ingredientsTab[i].style.display = 'none';
+        ingredientsButton[i].style.backgroundColor = '#F4900C';
     }
     
     for (i = 0; i < methodTab.length; i++) {
         methodTab[i].style.display = 'block';
+        methodTab[i].style.backgroundColor = '#FFCC4D';
+        methodButton[i].style.backgroundColor = '#FFCC4D';
     }
-}
-let methodButton = document.getElementsByClassName('methodbutton')
+
+    }
+
 for (i = 0; i < methodButton.length; i++) {
     methodButton[i].addEventListener('click', openMethod)
 }
@@ -25,19 +31,20 @@ for (i = 0; i < methodButton.length; i++) {
 function openIngredients() {
     for (i = 0; i < methodTab.length; i++) {
         methodTab[i].style.display = 'none';
+        methodButton[i].style.backgroundColor = '#F4900C';
     }
     
     for (i = 0; i < methodTab.length; i++) {
         ingredientsTab[i].style.display = 'block';
+        ingredientsTab[i].style.backgroundColor = '#FFCC4D';
+        ingredientsButton[i].style.backgroundColor = '#FFCC4D';
     }
 }
-let ingredientsButton = document.getElementsByClassName('ingredientsbutton')
+
     for (i = 0; i < ingredientsButton.length; i++) {
         ingredientsButton[i].addEventListener('click', openIngredients)
     }
     
-    
- 
     
     
     })
